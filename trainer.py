715afda7a4e10995821180bc35c0896d7da47a5c
zhendong <zw2533@columbia.edu>
2021-10-04 00:03:15 -0500

    fix bug

--- trainer.py
+++ trainer.py
@@ -6,6 +6,7 @@ from utils.logger import logger
 from utils.logger import create_stats_ordered_dict
 from utils import utils
 
+
 class Trainer(object):
     def __init__(self, agent, expl_env, eval_env, replay_buffer, device, start_timesteps=25e3):
         self.agent = agent
@@ -46,8 +47,6 @@ class Trainer(object):
 
                 if total_timesteps >= self.start_timesteps:
                     gf1_loss, gf2_loss, actor_loss, log_pi = self.agent.train_from_batch(self.replay_buffer)
-                else:
-                    gf1_loss, gf2_loss, actor_loss, log_pi = 0., 0., 0., 0.
 
                 if done:
                     # +1 to account for 0 indexing. +0 on ep_timesteps since it will increment +1 even if done=True
@@ -61,18 +60,19 @@ class Trainer(object):
 
             # TODO: add code
             # Evaluate episode
-            utils.print_banner(f"Train step: {total_timesteps}", separator="*", num_star=90)
-            eval_res = self.eval_policy()
-            evaluations.append(eval_res)
-            # np.save(os.path.join(output_dir, "eval"), evaluations)
-            logger.record_tabular('Training Epochs', curr_epoch)
-            logger.record_tabular('GF1 Loss', gf1_loss.cpu().data.numpy())
-            logger.record_tabular('GF2 Loss', gf2_loss.cpu().data.numpy())
-            logger.record_tabular('Actor Loss', actor_loss.cpu().data.numpy())
-            logger.record_tabular('Policy log_pi', log_pi.cpu().data.numpy())
-            logger.record_tabular('Average Episodic Reward', eval_res)
+            if total_timesteps >= self.start_timesteps:
+                utils.print_banner(f"Train step: {total_timesteps}", separator="*", num_star=90)
+                eval_res = self.eval_policy()
+                evaluations.append(eval_res)
+                # np.save(os.path.join(output_dir, "eval"), evaluations)
+                logger.record_tabular('Training Epochs', curr_epoch)
+                logger.record_tabular('GF1 Loss', gf1_loss.cpu().data.numpy())
+                logger.record_tabular('GF2 Loss', gf2_loss.cpu().data.numpy())
+                logger.record_tabular('Actor Loss', actor_loss.cpu().data.numpy())
+                logger.record_tabular('Policy log_pi', log_pi.cpu().data.numpy())
+                logger.record_tabular('Average Episodic Reward', eval_res)
 
-            logger.dump_tabular()
+                logger.dump_tabular()
 
     # Runs policy for X episodes and returns average reward
     # A fixed seed is used for the eval environment
