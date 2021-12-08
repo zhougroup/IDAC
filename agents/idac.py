6f9c29de6d2649a1446f993a6d6abdceff884c08
Zhendong <zw2533@columbia.edu>
2021-12-08 10:11:17 -0800

    change log_prob_max value

--- agents/idac.py
+++ agents/idac.py
@@ -7,8 +7,8 @@ from utils.distributions import TanhNormal
 
 LOG_SIG_MIN = -20.
 LOG_SIG_MAX = 2.
-LOG_PROB_MIN = -10.
-LOG_PROB_MAX = 10.
+LOG_PROB_MIN = -5.
+LOG_PROB_MAX = 5.
 EPS = 1e-6
 
 class G_Actor(nn.Module):
